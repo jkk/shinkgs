62286dadcd0c8996ac099e19ce6e21898b63e740
Adam <adamreifsneider@gmail.com>
2018-05-18 16:54:59 -0400

    reuse css and move tooltip down a little

--- src/ui/user/UserName.js
+++ src/ui/user/UserName.js
@@ -28,15 +28,15 @@ export default class UserName extends Component {
     let icons = (
       <div className='UserName-icons'>
         {flags.robot ?
-          <div className='UserName-robot'> 🤖<div className='UserIcons-icon-tooltip'>Robot</div></div> : null}
+          <div className='UserName-robot UserIcons-icon'> 🤖<div className='UserIcons-icon-tooltip'>Robot</div></div> : null}
         {flags.selfish ?
           <div className='UserName-selfish'>
-            <div className='UserName-selfish-icon'>~<div className='UserIcons-icon-tooltip'>Selfish</div></div>
+            <div className='UserName-selfish-icon UserIcons-icon'>~<div className='UserIcons-icon-tooltip'>Selfish</div></div>
           </div> :
           null}
         {flags.guest ?
           <div className='UserName-guest'>
-            <div className='UserName-selfish-icon'> 👤<div className='UserIcons-icon-tooltip'>Guest</div></div>
+            <div className='UserName-selfish-icon UserIcons-icon'> 👤<div className='UserIcons-icon-tooltip'>Guest</div></div>
           </div> :
           null}
         {extraIcons ? <UserIcons user={user} /> : null}
